feebb2c3f8a7c961a34213568f435a4f0536adce
HarryWu-CHN <904714159@qq.com>
2021-01-08 23:25:49 +0800

    mallocTest

---
+++ xv6/mallocTest.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(){
+	void *arr[100];
+	for (int i=0; i<100; i++){
+		arr[i] = malloc(100);
+	}
+	for (int i=0; i<100; i++){
+		free(arr[i]);
+	}
+	printf(1, "mallocTest Has Done!\n");
+	return 0;
+}

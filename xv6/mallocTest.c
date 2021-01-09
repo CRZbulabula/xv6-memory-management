018c8aee51506c21d1cf94079479fd31842ecf52
gjf1067311025 <73088539+gjf1067311025@users.noreply.github.com>
2021-01-09 22:42:03 +0800

    malloc 最终版

--- xv6/mallocTest.c
+++ xv6/mallocTest.c
@@ -3,14 +3,27 @@
 #include "user.h"
 
 int main(){
-	void *arr[100];
-	for (int i=0; i<100; i++){
-		arr[i] = malloc(100);
-	}
-	for (int i=0; i<100; i++){
-		free(arr[i]);
-	}
+	void *arr[20];
+	printf(1, "mallocTest Starts.\n");
+	arr[0]=malloc(50);
+	arr[1]=malloc(100);
+	arr[2]=malloc(80);
+	arr[3]=malloc(160);
+	arr[4]=malloc(20);
+	free(arr[0]);
+	free(arr[3]);
+	arr[5]=malloc(40);
+	free(arr[2]);
+	arr[6]=malloc(200);
+	arr[7]=malloc(60);
+	free(arr[5]);
+	arr[8]=malloc(120);
+	arr[9]=malloc(50);
+	
+	// for (int i=0; i<1; i++){
+	// 	free( (void*)arr[9]);
+	// }
 	printf(1, "mallocTest Has Done!\n");
-	printf(1, "The unused piece is %d bytes",total_piece());
+	printf(1, "The unused piece is %d bytes!\n",total_piece());
 	return 0;
 }
